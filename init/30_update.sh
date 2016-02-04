6a098bf1f6201eae950ee8429dccaf0bdad57cb1
kylerw <kylerw@gmail.com>
2016-02-04 12:40:46 -0700

    install dependencies

---
+++ init/30_update.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+mkdir -p /config/{speedtest-tracking,speedtest-cli}
+
+[[ ! -d /config/speedtest-tracking/.git ]] && (git https://github.com/kylerw/speedtest-tracking.git /config/speedtest-tracking && \
+[[ ! -d /config/speedtest-cli/.git ]] && (git https://github.com/sivel/speedtest-cli.git /config/speedtest-cli && \
+
+chown -R abc:abc /config)
+
+# opt out for autoupdates
+[ "$ADVANCED_DISABLEUPDATES" ] && exit 0
+
+cd /config/speedtest-tracking
+git pull
+
+cd /config/speedtest-cli
+git pull
+
+chown -R abc:abc /config
