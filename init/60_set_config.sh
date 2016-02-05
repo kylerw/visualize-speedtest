e17c525bb13c1f1cb22984bf1d236f5d5db1a290
kylerw <kylerw@gmail.com>
2016-02-04 20:08:32 -0700

    Update 60_set_config.sh

--- init/60_set_config.sh
+++ init/60_set_config.sh
@@ -4,7 +4,11 @@ if [ ! -f /config/speedtest-tracking/config.py ]; then
   cp /config/speedtest-tracking/config-example.py /config/speedtest-tracking/config.py
 fi
 
-cp /config/speedtest-tracking/speedtest_cron-example /etc/cron.hourly/speedtest_cron
-chmod 777 /etc/cron.hourly/speedtest_cron
 
+if [ ! -f /config/speedtest-tracking/speedtest_cron ]; then
+  cp /config/speedtest-tracking/speedtest_cron-example /config/speedtest-tracking/speedtest_cron
+  cp /config/speedtest-tracking/speedtest_cron /etc/cron.hourly/speedtest_cron
+fi
+
+chmod 777 /etc/cron.hourly/speedtest_cron
 chown abc:abc -R /config
